5d8e0670f2247c45f42bda218f63c07daf5bb7e0
arav-sharma <142256252+arav-sharma@users.noreply.github.com>
2023-09-06 11:39:06 -0500

    Update script.js
    updated math to parse int

--- script.js
+++ script.js
@@ -1,35 +1,35 @@
 function calculateGPA(){
     // Calculates the difference in class grades from the max achievable grade of 100
-    var classGrade1 = ((100-document.getElementById("grade1").value)*0.05);
-    var classGrade2 = ((100-document.getElementById("grade2").value)*0.05);
-    var classGrade3 = ((100-document.getElementById("grade3").value)*0.05);
-    var classGrade4 = ((100-document.getElementById("grade4").value)*0.05);
-    var classGrade5 = ((100-document.getElementById("grade5").value)*0.05);
-    var classGrade6 = ((100-document.getElementById("grade6").value)*0.05);
-    var classGrade7 = ((100-document.getElementById("grade7").value)*0.05);
+    var classGrade1 = parseInt(((100-document.getElementById("grade1").value)*0.05));
+    var classGrade2 = parseInt(((100-document.getElementById("grade2").value)*0.05));
+    var classGrade3 = parseInt(((100-document.getElementById("grade3").value)*0.05));
+    var classGrade4 = parseInt(((100-document.getElementById("grade4").value)*0.05));
+    var classGrade5 = parseInt(((100-document.getElementById("grade5").value)*0.05));
+    var classGrade6 = parseInt(((100-document.getElementById("grade6").value)*0.05));
+    var classGrade7 = parseInt(((100-document.getElementById("grade7").value)*0.05));
 
     // Subtracts grade difference (above) from GPA difference from max achievable GPA 
     // (dependent on weight value: 5, 4.5, 4)
-    var classGPA1 = (document.getElementById("weight1").value - classGrade1);
-    var classGPA2 = (document.getElementById("weight2").value - classGrade2);
-    var classGPA3 = (document.getElementById("weight3").value - classGrade3);
-    var classGPA4 = (document.getElementById("weight4").value - classGrade4);
-    var classGPA5 = (document.getElementById("weight5").value - classGrade5);
-    var classGPA6 = (document.getElementById("weight6").value - classGrade6);
-    var classGPA7 = (document.getElementById("weight7").value - classGrade7);
+    var classGPA1 = parseInt((document.getElementById("weight1").value - classGrade1));
+    var classGPA2 = parseInt((document.getElementById("weight2").value - classGrade2));
+    var classGPA3 = parseInt((document.getElementById("weight3").value - classGrade3));
+    var classGPA4 = parseInt((document.getElementById("weight4").value - classGrade4));
+    var classGPA5 = parseInt((document.getElementById("weight5").value - classGrade5));
+    var classGPA6 = parseInt((document.getElementById("weight6").value - classGrade6));
+    var classGPA7 = parseInt((document.getElementById("weight7").value - classGrade7));
 
     // Same as above but weight is set to 4 for unweighted
-    var classGPA1UW = (4 - classGrade1);
-    var classGPA2UW = (4 - classGrade2);
-    var classGPA3UW = (4 - classGrade3);
-    var classGPA4UW = (4 - classGrade4);
-    var classGPA5UW = (4 - classGrade5);
-    var classGPA6UW = (4 - classGrade6);
-    var classGPA7UW = (4 - classGrade7);
+    var classGPA1UW = parseInt((4 - classGrade1));
+    var classGPA2UW = parseInt((4 - classGrade2));
+    var classGPA3UW = parseInt((4 - classGrade3));
+    var classGPA4UW = parseInt((4 - classGrade4));
+    var classGPA5UW = parseInt((4 - classGrade5));
+    var classGPA6UW = parseInt((4 - classGrade6));
+    var classGPA7UW = parseInt((4 - classGrade7));
 
     // Calculates average weighted & unweighted GPAs
-    var totalGPA = ((classGPA1+classGPA2+classGPA3+classGPA4+classGPA5+classGPA6+classGPA7)/7)
-    var totalUWGPA = ((classGPA1UW+classGPA2UW+classGPA3UW+classGPA4UW+classGPA5UW+classGPA6UW+classGPA7UW)/7)
+    var totalGPA = parseInt(((classGPA1+classGPA2+classGPA3+classGPA4+classGPA5+classGPA6+classGPA7)/7));
+    var totalUWGPA = parseInt(((classGPA1UW+classGPA2UW+classGPA3UW+classGPA4UW+classGPA5UW+classGPA6UW+classGPA7UW)/7));
     
     // Displays calculated GPA to website
     document.getElementById("header4").innerHTML="<u>Calculated GPAs:</u>";
